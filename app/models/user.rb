20dee1da3ae369d99a7a5455efd5806f5eea5df3
aiit1928ht <a1928ht@aiit.ac.jp>
2019-06-03 21:00:11 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token
     before_save { email.downcase! }
     validates :name,  presence: true, length: { maximum: 50 }
@@ -8,19 +9,19 @@ class User < ApplicationRecord
                 uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-
+    
     # 渡された文字列のハッシュ値を返す
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
     end
-
+    
     # ランダムなトークンを返す
     def User.new_token
         SecureRandom.urlsafe_base64
     end
-
+    
     # 永続セッションのためにユーザーをデータベースに記憶する
     def remember
         self.remember_token = User.new_token
@@ -31,9 +32,15 @@ class User < ApplicationRecord
     def authenticated?(remember_token)
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
-
+    
     # ユーザーのログイン情報を破棄する
     def forget
         update_attribute(:remember_digest, nil)
+    end
+    
+    # 試作feedの定義
+    # 完全な実装は次章の「ユーザーをフォローする」を参照
+    def feed
+        Micropost.where("user_id = ?", id)
     end
 end
